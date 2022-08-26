c1ebbc1afb5a691f0f6a6ee786017c5b01b008e7
thuytien1510 <thuytiencoder@gmail.com>
2022-08-26 13:49:42 +0700

    test

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+import React, { useEffect, useState } from 'react';
+import ListArtical from './components/ListArtical/ListArtical';
+import Pagnination from './components/Pagination/Pagnination';
+
+function App() {
+  const [listArticles, setlistArticles] = useState([]);
+  const [page, setPage] = useState(0);
+
+  useEffect(() => {
+    getData();
+  }, [page]);
+
+  const getData = () => {
+    fetch(`https://api.realworld.io/api/articles/?limit=10&offset=${page}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setlistArticles(data.articles)
+      })
+  }
+
+  return (
+    <div className="App">
+      <ListArtical
+        listArticles={listArticles}
+      />
+      <Pagnination
+        numberPage={page}
+        setPage={setPage}
+      />
+    </div>
+  );
+}
+
+export default App;
